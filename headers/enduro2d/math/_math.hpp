9564ca6a03a0c1f14513278d2a745e077939d3b0
BlackMATov <blackmatov@gmail.com>
2018-08-08 18:42:23 +0700

    basic math functions wip

--- headers/enduro2d/math/_math.hpp
+++ headers/enduro2d/math/_math.hpp
@@ -7,3 +7,274 @@
 #pragma once
 
 #include "../base/_all.hpp"
+
+namespace e2d { namespace math
+{
+    //
+    // numeric cast
+    //
+
+    namespace impl
+    {
+        // f -> f
+        template < typename To, typename From >
+        std::enable_if_t<
+            std::is_floating_point<From>::value && std::is_floating_point<To>::value
+        , bool>
+        check_numeric_cast(From v) noexcept {
+            const To l = std::numeric_limits<To>::lowest();
+            const To m = std::numeric_limits<To>::max();
+            return v >= l && v <= m;
+        }
+
+        // i/u -> f
+        template < typename To, typename From >
+        std::enable_if_t<
+            std::is_integral<From>::value && std::is_floating_point<To>::value
+        , bool>
+        check_numeric_cast(From) noexcept {
+            return true;
+        }
+
+        // f -> i/u
+        template < typename To, typename From >
+        std::enable_if_t<
+            std::is_floating_point<From>::value && std::is_integral<To>::value
+        , bool>
+        check_numeric_cast(From v) noexcept {
+            const To l = std::numeric_limits<To>::lowest();
+            const To m = std::numeric_limits<To>::max();
+            return
+                v >= static_cast<From>(l) &&
+                v <= static_cast<From>(m);
+        }
+
+        // i -> i, u -> u
+        template < typename To, typename From >
+        std::enable_if_t<
+            std::is_signed<From>::value == std::is_signed<To>::value &&
+            std::is_integral<From>::value && std::is_integral<To>::value
+        , bool>
+        check_numeric_cast(From v) noexcept {
+            const To l = std::numeric_limits<To>::lowest();
+            const To m = std::numeric_limits<To>::max();
+            return v >= l && v <= m;
+        }
+
+        // i -> u
+        template < typename To, typename From >
+        std::enable_if_t<
+            std::is_signed<From>::value && std::is_unsigned<To>::value &&
+            std::is_integral<From>::value && std::is_integral<To>::value
+        , bool>
+        check_numeric_cast(From v) noexcept {
+            const To m = std::numeric_limits<To>::max();
+            return v >= 0 && static_cast<std::make_unsigned_t<From>>(v) <= m;
+        }
+
+        // u -> i
+        template < typename To, typename From >
+        std::enable_if_t<
+            std::is_unsigned<From>::value && std::is_signed<To>::value &&
+            std::is_integral<From>::value && std::is_integral<To>::value
+        , bool>
+        check_numeric_cast(From v) noexcept {
+            const To m = std::numeric_limits<To>::max();
+            return v <= static_cast<std::make_unsigned_t<To>>(m);
+        }
+    }
+
+    template < typename To, typename From >
+    std::enable_if_t<
+        std::is_arithmetic<From>::value &&
+        std::is_arithmetic<To>::value, To>
+    numeric_cast(From v) noexcept {
+        E2D_ASSERT(impl::check_numeric_cast<To>(v));
+        return static_cast<To>(v);
+    }
+
+    //
+    // default precision
+    //
+
+    template < typename T >
+    std::enable_if_t<std::is_integral<T>::value, T>
+    default_precision() noexcept {
+        return T(0);
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_same<T, f32>::value, T>
+    default_precision() noexcept {
+        return 0.00001f;
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_same<T, f64>::value, T>
+    default_precision() noexcept {
+        return 0.0000001;
+    }
+
+    //
+    // power of two
+    //
+
+    template < typename T >
+    std::enable_if_t<std::is_unsigned<T>::value, bool>
+    is_power_of_2(T v) noexcept {
+        return v && !(v & (v - 1));
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_unsigned<T>::value, T>
+    max_power_of_2() noexcept {
+        return T(1) << (sizeof(T) * 8 - 1);
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_unsigned<T>::value, T>
+    next_power_of_2(T v) noexcept {
+        E2D_ASSERT(v <= max_power_of_2<T>());
+        if ( v == T(0) ) {
+            return T(1);
+        }
+        --v;
+        u32 i = 1;
+        do {
+            v |= v >> i;
+            i <<= 1;
+        } while (v & (v + 1));
+        return v + 1;
+    }
+
+    //
+    // abs
+    //
+
+    template < typename T >
+    std::enable_if_t<std::is_signed<T>::value, T>
+    abs(T v) noexcept {
+        return v < T(0) ? -v : v;
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_unsigned<T>::value, T>
+    abs(T v) noexcept {
+        return v;
+    }
+
+    //
+    // sqrt
+    //
+
+    template < typename T >
+    std::enable_if_t<std::is_floating_point<T>::value, T>
+    sqrt(T v) noexcept {
+        return std::sqrt(v);
+    }
+
+    //
+    // ceil/floor/round
+    //
+
+    template < typename T >
+    std::enable_if_t<std::is_integral<T>::value, T>
+    ceil(T v) noexcept {
+        return v;
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_integral<T>::value, T>
+    floor(T v) noexcept {
+        return v;
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_integral<T>::value, T>
+    round(T v) noexcept {
+        return v;
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_floating_point<T>::value, T>
+    ceil(T v) noexcept {
+        return std::ceil(v);
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_floating_point<T>::value, T>
+    floor(T v) noexcept {
+        return std::floor(v);
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_floating_point<T>::value, T>
+    round(T v) noexcept {
+        return std::round(v);
+    }
+
+    //
+    // min/max
+    //
+
+    template < typename T >
+    std::enable_if_t<std::is_arithmetic<T>::value, T>
+    min(T v) noexcept {
+        return v;
+    }
+
+    template < typename T1, typename T2, typename... Ts >
+    std::common_type_t<T1, T2, Ts...> min(T1 v1, T2 v2, Ts... vs) noexcept {
+        return min(v1 < v2 ? v1 : v2, vs...);
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_arithmetic<T>::value, T>
+    max(T v) noexcept {
+        return v;
+    }
+
+    template < typename T1, typename T2, typename... Ts >
+    std::common_type_t<T1, T2, Ts...> max(T1 v1, T2 v2, Ts... vs) noexcept {
+        return max(v1 < v2 ? v2 : v1, vs...);
+    }
+
+    //
+    // clamp
+    //
+
+    template < typename T >
+    std::enable_if_t<std::is_arithmetic<T>::value, T>
+    clamp(T v, T vmin, T vmax) noexcept {
+        E2D_ASSERT(vmin <= vmax);
+        return min(max(v, vmin), vmax);
+    }
+
+    template < typename T >
+    std::enable_if_t<std::is_arithmetic<T>::value, T>
+    clamp01(T v) noexcept {
+        return clamp(v, T(0), T(1));
+    }
+
+    //
+    // is_near_zero
+    //
+
+    template < typename T >
+    std::enable_if_t<std::is_arithmetic<T>::value, bool>
+    is_near_zero(T v, T precision = default_precision<T>()) noexcept {
+        return abs(v) <= precision;
+    }
+
+    //
+    // approximately
+    //
+
+    template < typename T >
+    std::enable_if_t<std::is_arithmetic<T>::value, bool>
+    approximately(T l, T r, T precision = default_precision<T>()) noexcept {
+        return l < r
+            ? (r - l) <= precision
+            : (l - r) <= precision;
+    }
+}}
--- untests/sources/untests_math/_math.cpp
+++ untests/sources/untests_math/_math.cpp
@@ -8,4 +8,197 @@
 #include "_math.hpp"
 
 TEST_CASE("math") {
+    {
+        REQUIRE(math::numeric_cast<u32>(1) == 1u);
+        REQUIRE(math::numeric_cast<i32>(1.2f) == 1);
+        REQUIRE(math::numeric_cast<i32>(-1.2f) == -1);
+        REQUIRE(math::numeric_cast<i32>(1.8f) == 1);
+        REQUIRE(math::numeric_cast<i32>(-1.8f) == -1);
+    }
+    {
+        using math::impl::check_numeric_cast;
+
+        // f -> f
+        REQUIRE(check_numeric_cast<f64>(std::numeric_limits<f64>::max()));
+        REQUIRE(check_numeric_cast<f64>(std::numeric_limits<f64>::lowest()));
+        REQUIRE(check_numeric_cast<f64>(std::numeric_limits<f32>::max()));
+        REQUIRE(check_numeric_cast<f64>(std::numeric_limits<f32>::lowest()));
+        REQUIRE_FALSE(check_numeric_cast<f32>(std::numeric_limits<f64>::max()));
+        REQUIRE_FALSE(check_numeric_cast<f32>(std::numeric_limits<f64>::lowest()));
+        REQUIRE_FALSE(check_numeric_cast<f64>(std::numeric_limits<f64>::infinity()));
+        REQUIRE_FALSE(check_numeric_cast<f64>(std::numeric_limits<f64>::quiet_NaN()));
+        REQUIRE_FALSE(check_numeric_cast<f64>(std::numeric_limits<f64>::signaling_NaN()));
+
+        // i -> f
+        REQUIRE(check_numeric_cast<f32>(std::numeric_limits<int>::min()));
+        REQUIRE(check_numeric_cast<f32>(std::numeric_limits<int>::max()));
+        REQUIRE(check_numeric_cast<f64>(std::numeric_limits<int>::min()));
+        REQUIRE(check_numeric_cast<f64>(std::numeric_limits<int>::max()));
+
+        // f -> i
+        REQUIRE(check_numeric_cast<i32>(1.f));
+        REQUIRE(check_numeric_cast<i32>(-1.f));
+        REQUIRE_FALSE(check_numeric_cast<i32>(std::numeric_limits<f32>::max()));
+        REQUIRE_FALSE(check_numeric_cast<i32>(std::numeric_limits<f32>::lowest()));
+        REQUIRE_FALSE(check_numeric_cast<i32>(std::numeric_limits<f32>::infinity()));
+        REQUIRE_FALSE(check_numeric_cast<i32>(std::numeric_limits<f32>::quiet_NaN()));
+        REQUIRE_FALSE(check_numeric_cast<i32>(std::numeric_limits<f32>::signaling_NaN()));
+
+        // f -> u
+        REQUIRE(check_numeric_cast<u32>(1.f));
+        REQUIRE_FALSE(check_numeric_cast<u32>(-1.f));
+        REQUIRE_FALSE(check_numeric_cast<u32>(std::numeric_limits<f32>::max()));
+        REQUIRE_FALSE(check_numeric_cast<u32>(std::numeric_limits<f32>::lowest()));
+        REQUIRE_FALSE(check_numeric_cast<u32>(std::numeric_limits<f32>::infinity()));
+        REQUIRE_FALSE(check_numeric_cast<u32>(std::numeric_limits<f32>::quiet_NaN()));
+        REQUIRE_FALSE(check_numeric_cast<u32>(std::numeric_limits<f32>::signaling_NaN()));
+
+        // i -> i
+        REQUIRE(check_numeric_cast<i8>(-128));
+        REQUIRE(check_numeric_cast<i8>(127));
+        REQUIRE(check_numeric_cast<i16>(i8(-128)));
+        REQUIRE(check_numeric_cast<i16>(i8(127)));
+        REQUIRE_FALSE(check_numeric_cast<i8>(-129));
+        REQUIRE_FALSE(check_numeric_cast<i8>(128));
+        REQUIRE_FALSE(check_numeric_cast<i16>(std::numeric_limits<i32>::max()));
+        REQUIRE_FALSE(check_numeric_cast<i16>(std::numeric_limits<i32>::lowest()));
+        REQUIRE(check_numeric_cast<i32>(std::numeric_limits<i32>::max()));
+        REQUIRE(check_numeric_cast<i32>(std::numeric_limits<i32>::lowest()));
+
+        // u -> u
+        REQUIRE(check_numeric_cast<u8>(255u));
+        REQUIRE_FALSE(check_numeric_cast<u8>(256u));
+        REQUIRE_FALSE(check_numeric_cast<u16>(std::numeric_limits<u32>::max()));
+        REQUIRE(check_numeric_cast<u16>(std::numeric_limits<u32>::lowest()));
+        REQUIRE(check_numeric_cast<u32>(std::numeric_limits<u32>::max()));
+        REQUIRE(check_numeric_cast<u32>(std::numeric_limits<u32>::lowest()));
+
+        // i -> u
+        REQUIRE(check_numeric_cast<u8>(255));
+        REQUIRE_FALSE(check_numeric_cast<u8>(-1));
+        REQUIRE_FALSE(check_numeric_cast<u8>(256));
+
+        // u -> i
+        REQUIRE(check_numeric_cast<i8>(127u));
+        REQUIRE_FALSE(check_numeric_cast<i8>(128u));
+    }
+    {
+        REQUIRE(math::is_power_of_2(1u));
+        REQUIRE(math::is_power_of_2(2u));
+        REQUIRE(math::is_power_of_2(4u));
+        REQUIRE(math::is_power_of_2(8u));
+        REQUIRE_FALSE(math::is_power_of_2(0u));
+        REQUIRE_FALSE(math::is_power_of_2(3u));
+        REQUIRE_FALSE(math::is_power_of_2(6u));
+        REQUIRE_FALSE(math::is_power_of_2(10u));
+
+        REQUIRE(math::max_power_of_2<u8>()  == (1u << 7));
+        REQUIRE(math::max_power_of_2<u16>() == (1u << 15));
+        REQUIRE(math::max_power_of_2<u32>() == (1u << 31));
+
+        REQUIRE(math::next_power_of_2(u8(0)) == u8(1));
+        REQUIRE(math::next_power_of_2(u8(1)) == u8(1));
+        REQUIRE(math::next_power_of_2(u8(2)) == u8(2));
+        REQUIRE(math::next_power_of_2(u16(3)) == u16(4));
+        REQUIRE(math::next_power_of_2(u16(4)) == u16(4));
+        REQUIRE(math::next_power_of_2(u32(5)) == u32(8));
+        REQUIRE(math::next_power_of_2(u32(6)) == u32(8));
+        REQUIRE(math::next_power_of_2(u32(8)) == u32(8));
+        REQUIRE(math::next_power_of_2(u32(9)) == u32(16));
+    }
+    {
+        REQUIRE(math::abs(2) == 2);
+        REQUIRE(math::abs(3u) == 3u);
+        REQUIRE(math::abs(-4) == 4);
+
+        REQUIRE(math::approximately(math::abs(1.2f), 1.2f));
+        REQUIRE(math::approximately(math::abs(-1.2f), 1.2f));
+
+        REQUIRE(math::approximately(math::abs(2.3), 2.3));
+        REQUIRE(math::approximately(math::abs(-2.3), 2.3));
+    }
+    {
+        REQUIRE(math::approximately(math::ceil(1), 1));
+        REQUIRE(math::approximately(math::floor(1), 1));
+        REQUIRE(math::approximately(math::round(1), 1));
+
+        REQUIRE(math::approximately(math::ceil(-1), -1));
+        REQUIRE(math::approximately(math::floor(-1), -1));
+        REQUIRE(math::approximately(math::round(-1), -1));
+
+        REQUIRE(math::approximately(math::ceil(0.4f), 1.f));
+        REQUIRE(math::approximately(math::floor(0.4f), 0.f));
+        REQUIRE(math::approximately(math::ceil(-0.4f), 0.f));
+        REQUIRE(math::approximately(math::floor(-0.4f), -1.f));
+
+        REQUIRE(math::approximately(math::ceil(0.6f), 1.f));
+        REQUIRE(math::approximately(math::floor(0.6f), 0.f));
+        REQUIRE(math::approximately(math::ceil(-0.6f), 0.f));
+        REQUIRE(math::approximately(math::floor(-0.6f), -1.f));
+
+        REQUIRE(math::approximately(math::round(0.4f), 0.f));
+        REQUIRE(math::approximately(math::round(0.6f), 1.f));
+        REQUIRE(math::approximately(math::round(-0.4f), 0.f));
+        REQUIRE(math::approximately(math::round(-0.6f), -1.f));
+    }
+    {
+        REQUIRE(math::min(3) == 3);
+        REQUIRE(math::min(1,1) == 1);
+        REQUIRE(math::min(1,2) == 1);
+        REQUIRE(math::min(2,1) == 1);
+        REQUIRE(math::min(1,2,3) == 1);
+        REQUIRE(math::min(3,2,1) == 1);
+
+        REQUIRE(math::max(3) == 3);
+        REQUIRE(math::max(1,1) == 1);
+        REQUIRE(math::max(1,2) == 2);
+        REQUIRE(math::max(2,1) == 2);
+        REQUIRE(math::max(1,2,3) == 3);
+        REQUIRE(math::max(3,2,1) == 3);
+
+        REQUIRE(math::clamp(2,1,3) == 2);
+        REQUIRE(math::clamp(3,1,3) == 3);
+        REQUIRE(math::clamp(4,1,3) == 3);
+        REQUIRE(math::clamp(1,1,3) == 1);
+        REQUIRE(math::clamp(0,1,3) == 1);
+
+        REQUIRE(math::clamp01(2) == 1);
+        REQUIRE(math::clamp01(-1) == 0);
+        REQUIRE(math::clamp01(0) == 0);
+        REQUIRE(math::clamp01(1) == 1);
+    }
+    {
+        REQUIRE(math::is_near_zero(0));
+        REQUIRE_FALSE(math::is_near_zero(1));
+        REQUIRE_FALSE(math::is_near_zero(1u));
+        REQUIRE(math::is_near_zero(1, 1));
+        REQUIRE(math::is_near_zero(-1, 1));
+        REQUIRE(math::is_near_zero(1u, 1u));
+
+        REQUIRE(math::is_near_zero(0.000001f));
+        REQUIRE(math::is_near_zero(-0.000001f));
+        REQUIRE_FALSE(math::is_near_zero(0.0001f));
+        REQUIRE_FALSE(math::is_near_zero(-0.0001f));
+        REQUIRE(math::is_near_zero(0.0001f, 0.001f));
+        REQUIRE(math::is_near_zero(-0.0001f, 0.001f));
+    }
+    {
+        REQUIRE(math::approximately(1, 1));
+
+        REQUIRE(math::approximately(1, 2, 1));
+        REQUIRE(math::approximately(1, 3, 2));
+        REQUIRE(math::approximately(1, 3, 3));
+
+        REQUIRE(math::approximately(2, 1, 1));
+        REQUIRE(math::approximately(3, 1, 2));
+        REQUIRE(math::approximately(3, 1, 3));
+
+        REQUIRE_FALSE(math::approximately(1, 2));
+        REQUIRE_FALSE(math::approximately(1, 2, 0));
+        REQUIRE_FALSE(math::approximately(1, 3, 1));
+
+        REQUIRE_FALSE(math::approximately(2, 1));
+        REQUIRE_FALSE(math::approximately(2, 1, 0));
+        REQUIRE_FALSE(math::approximately(3, 1, 1));
+    }
 }
