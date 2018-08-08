fe69a43d589ac46689385fb4cd4f0acd05dadfdf
BlackMATov <blackmatov@gmail.com>
2018-08-08 15:54:21 +0700

    remove pnt2 and pnt3 source files

--- headers/enduro2d/math/pnt3.hpp
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************
- * This file is part of the "Enduro2D"
- * For conditions of distribution and use, see copyright notice in LICENSE.md
- * Copyright (C) 2018 Matvey Cherevko
- ******************************************************************************/
-
-#pragma once
-
-#include "_math.hpp"
